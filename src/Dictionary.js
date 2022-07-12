fcf19a1c9e5e06e60d46fdf01f91a2d945413ee7
KaiqiGeorge <oliviakaiqihuang@gmail.com>
2022-07-12 11:56:03 -0700

    added the dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,27 +3,36 @@ import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-    const[keyword,setKeyword]=useState("");
-    const[dictionary, setDictionary]=useState("");
+  const [keyword, setKeyword] = useState("");
+  const [dictionary, setDictionary] = useState("");
 
-    function handleKeyword(event){
-        event.preventDefault();
+  function handleResponse(response){
+    console.log(response.data);
+  }
 
-    }
-    function search(){
-        
-    }
+  function search() {
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-    function updateKeyword(event){
-        setKeyword(event.target.value);
-        search();
-    }
+  function updateKeyword(event) {
+    setKeyword(event.target.value);
+    search();
+  }
+
+  function handleKeyword(event) {
+    event.preventDefault();
+  }
   return (
     <div className="dictionary">
       <h1>Dictionary</h1>
       <form className="row" onSubmit={handleKeyword}>
         <div className="col-8">
-          <input type="search" className="form-control" onChange={updateKeyword}/>
+          <input
+            type="search"
+            className="form-control"
+            onChange={updateKeyword}
+          />
         </div>
         <div className="col-4">
           <input type="submit" value="search" className="btn btn-primary" />
