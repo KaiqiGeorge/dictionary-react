67084ce3b9c6d5f6de5b9c07fc8ddb9f5cc38ec7
KaiqiGeorge <oliviakaiqihuang@gmail.com>
2022-07-12 19:17:38 -0700

    added footer

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="dictionary-app">
       <Dictionary defaultWord="sunset"/>
+      <footer> This <a href="https://github.com/KaiqiGeorge/dictionary-react" target="_blank"  rel="noreferrer">open-sourced</a> project was coded by Kaiqi George and hosted on<a href="https://kaiqi-dictionary-app.netlify.app/" target="_blank"  rel="noreferrer"> netlify</a> </footer>
     </div>
   );
 }
